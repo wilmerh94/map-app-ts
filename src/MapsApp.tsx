450e0607240959a9d38e4d8b971c33f126a1e3df
Wilmer <herrerawilmer.94@gmail.com>
2022-09-04 22:55:44 -0600

    starting with map app

---
+++ src/MapsApp.tsx
@@ -0,0 +1,13 @@
+import { PlacesProvider } from "./context"
+
+export const MapsApp = () => {
+   return (
+      <PlacesProvider>
+         <h1>
+
+
+            Hello World
+         </h1>
+      </PlacesProvider>
+   )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { MapsApp } from './MapsApp';
 
 const root = ReactDOM.createRoot(
   document.getElementById( 'root' ) as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MapsApp />
   </React.StrictMode>
 );
 
